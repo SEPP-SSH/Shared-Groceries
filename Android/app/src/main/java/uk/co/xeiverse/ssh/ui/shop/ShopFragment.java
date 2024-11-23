a40a205148864acd847cf85df59b17d38fc0dcd3
George Henderson <62885261+G-Henderson@users.noreply.github.com>
2024-11-23 15:48:01 +0000

    Created first basic version of the app
    Renamed the all the template files and added the correct

--- Android/app/src/main/java/uk/co/xeiverse/ssh/ui/shop/ShopFragment.java
+++ Android/app/src/main/java/uk/co/xeiverse/ssh/ui/shop/ShopFragment.java
@@ -1,4 +1,4 @@
-package uk.co.xeiverse.ssh.ui.notifications;
+package uk.co.xeiverse.ssh.ui.shop;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import uk.co.xeiverse.ssh.databinding.FragmentNotificationsBinding;
+import uk.co.xeiverse.ssh.databinding.FragmentShopBinding;
 
-public class NotificationsFragment extends Fragment {
+public class ShopFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentShopBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        ShopViewModel shopViewModel =
+                new ViewModelProvider(this).get(ShopViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentShopBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textShop;
+        shopViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
