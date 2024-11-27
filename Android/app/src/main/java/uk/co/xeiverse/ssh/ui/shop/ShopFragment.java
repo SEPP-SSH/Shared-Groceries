0f38aa04a9f31147d218aefc0159fcdc2b9e122d
George Henderson <62885261+G-Henderson@users.noreply.github.com>
2024-11-27 11:14:04 +0000

    Added to UI
    Added some new UI components for the grocery item browsing and trolley

--- Android/app/src/main/java/uk/co/xeiverse/ssh/ui/shop/ShopFragment.java
+++ Android/app/src/main/java/uk/co/xeiverse/ssh/ui/shop/ShopFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 import uk.co.xeiverse.ssh.databinding.FragmentShopBinding;
 
@@ -24,8 +28,32 @@ public class ShopFragment extends Fragment {
         binding = FragmentShopBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textShop;
-        shopViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        TabLayout tabLayout = binding.tabLayout;
+
+        String[] categories = {"OFFERS", "FROZEN", "FRUIT & VEG", "MEAT", "BAKERY", "DAIRY", "CLEANING", "PASTA & RICE"};
+
+        for (String category : categories) {
+            tabLayout.addTab(tabLayout.newTab().setText(category));
+        }
+
+        FloatingActionButton fab = binding.viewTrolleyIcon;
+        SearchView searchView = binding.searchView;
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fab.setVisibility(View.GONE);
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                fab.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
+
         return root;
     }
 
