f0c85aa70c49a51d96109272cb32909f3fa1c5e8
Harvey <harv1001@outlook.com>
2024-12-04 23:35:00 +0000

    Refactored `ReturnedBasket`
    Moved `ReturnedBasket` from being a nested class of `QueryServicer` to its own class to fix the bug that one cannot instantiate a nested class if in a static context (which the `returnBasketId` method is).

---
+++ SSH-Cloud/src/main/java/ssh/ReturnedBasket.java
@@ -0,0 +1,31 @@
+package ssh;
+
+import ssh.entities.BasketItem;
+
+import java.util.List;
+
+public class ReturnedBasket{
+    // attributes
+    private Integer basketid;
+    private List<BasketItem> basketItems;
+
+    // constructor
+    public ReturnedBasket(Integer basketid, List<BasketItem> basketItems){
+        this.basketid = basketid;
+        this.basketItems = basketItems;
+    }
+
+    // methods
+    public Integer getBasketid() {
+        return basketid;
+    }
+    public void setBasketid(Integer basketid) {
+        this.basketid = basketid;
+    }
+    public List<BasketItem> getBasketItems() {
+        return basketItems;
+    }
+    public void setBasketItems(List<BasketItem> basketItems) {
+        this.basketItems = basketItems;
+    }
+}
