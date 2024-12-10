28809b8925655eaea40f9a86e8de4b6c28963bd1
Harvey Nicholson <harv1001@outlook.com>
2024-12-10 12:27:33 +0000

    Added containerisation & basic orchestration
    Added a docker-compose file to run the MySQL server, build the SSH Cloud server .jar file & run it, and to then run the tests in the ClientFramework project.

--- ClientFramework/src/test/java/IntegrationTests.java
+++ ClientFramework/src/test/java/IntegrationTests.java
@@ -1,8 +1,21 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import ssh.entities.Basket;
+import ssh.entities.House;
+import ssh.entities.Store;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IntegrationTests {
+    @BeforeAll
+    static void setUp() throws Exception {
+        for (int counter = 0; counter < 90; counter++){
+            System.out.println("Waiting for SSH Cloud server to spin up. " + (90 - counter) + " seconds remaining.");
+            Thread.sleep(1000);
+        }
+    }
+
     @Test
     void placeholderTest() {
         assertTrue(true);
