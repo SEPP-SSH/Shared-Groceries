b0771630f8fd92bc6a5a60415204f072e9524a9a
Harvey Nicholson <harv1001@outlook.com>
2024-12-10 11:19:00 +0000

    Added a generic client framework
    Added a generic client framework that can be used to interact with the SSH Cloud server.
    
    This is the same framework that is used in SSH App.
    
    This has been added to allow for integration tests for SSH Cloud to be written in, and subsequently run automatically in a GitHub Action.

---
+++ ClientFramework/src/main/java/ssh/queryParameterWrappers/ReturnBasketIdQueryParameter.java
@@ -0,0 +1,28 @@
+package ssh.queryParameterWrappers;
+
+public class ReturnBasketIdQueryParameter {
+    // attributes
+    private int houseId;
+    private int storeId;
+
+    // constructors
+    public ReturnBasketIdQueryParameter(){} // need a default constructor for object deserialisation
+    public ReturnBasketIdQueryParameter(int houseId, int storeId){
+        this.houseId = houseId;
+        this.storeId = storeId;
+    }
+
+    // getters and setters
+    public int getHouseId() {
+        return houseId;
+    }
+    public void setHouseId(int houseId) {
+        this.houseId = houseId;
+    }
+    public int getStoreId() {
+        return storeId;
+    }
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+}
