d22b887c5ded306d52c50c8238fc42d32fb3e208
Harvey Nicholson <harv1001@outlook.com>
2024-12-10 19:02:44 +0000

    Update IntegrationTests.java
    Reduced the amount of time the integration tester works for the SSH Cloud server to spin up, reducing wasted time.

--- ClientFramework/src/test/java/IntegrationTests.java
+++ ClientFramework/src/test/java/IntegrationTests.java
@@ -17,8 +17,8 @@ public class IntegrationTests {
 
     @BeforeAll
     static void setUp() throws Exception {
-        for (int counter = 0; counter < 90; counter++){
-            System.out.println("Waiting for SSH Cloud server to spin up. " + (90 - counter) + " seconds remaining.");
+        for (int counter = 0; counter < 75; counter++){
+            System.out.println("Waiting for SSH Cloud server to spin up. " + (75 - counter) + " seconds remaining.");
             Thread.sleep(1000);
         }
 
